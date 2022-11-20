5d9872cdfeb52331318ab5efaf475741a3f775f1
salma <technicalsalma@gmail.com>
2022-11-20 23:23:52 +0600

    o customize the text field component

---
+++ src/components/Input.jsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import './Input.css'
+
+export default function Input() {
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      
+    </Box>
+  );
+}
